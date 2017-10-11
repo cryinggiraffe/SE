d922ed46a7bb04bf33f2c14708f5ca9406c1a81a
cryinggiraffe <490949304@qq.com>
2017-10-11 19:29:58 +0800

    User_new

--- src/main/java/User/User.java
+++ src/main/java/User/User.java
@@ -4,9 +4,6 @@ public class User {
 	
 	private String username;
 	private String password;
-	private String type;
-	
-	
 	
 	public User() {
 		super();
@@ -14,11 +11,10 @@ public class User {
 	}
 	
 	
-	public User(String username, String password, String type) {
+	public User(String username, String password) {
 		super();
 		this.username = username;
 		this.password = password;
-		this.type = type;
 	}
 
 
@@ -34,16 +30,12 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
+	
+	
 	
 	@Override
 	public String toString() {
-		return "User [username=" + username + ", password=" + password + ", type=" + type + "]";
+		return "User [username=" + username + ", password=" + password + "]";
 	}
 	
 	
