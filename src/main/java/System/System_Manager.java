6ffd71e1ae808e74b2c8e2680f3d51b18b8d00a2
cryinggiraffe <490949304@qq.com>
2017-10-11 20:17:04 +0800

    登陆\数据库操作函数\系统管理员

---
+++ src/main/java/System/System_Manager.java
@@ -0,0 +1,14 @@
+package System;
+
+import User.User;
+
+public class System_Manager extends User{
+	
+	private static String type = "系统管理员";
+
+	public static String getType() {
+		return type;
+	}
+
+	
+}
