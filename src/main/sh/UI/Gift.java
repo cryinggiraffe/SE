74713bb89ff2d8f1190626d8b3cb28d3ba00f5a7
luyao <yavenika@163.com>
2018-01-03 10:43:09 +0800

    ly

--- src/main/sh/UI/Gift.java
+++ src/main/sh/UI/Gift.java
@@ -1,4 +1,28 @@
 package UI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Gift {
+    private JFrame gframe;
+    private JPanel gpanel;
+    private JButton gbutton;
+
+    public  void createAndShowGiftGUI(){
+        gframe = new JFrame("赠送库存");
+        Container container = gframe.getContentPane();
+        gbutton = new JButton("赠品推荐");
+        gpanel = new JPanel();
+
+        container.setLayout(new FlowLayout());
+
+        gpanel.add(gbutton);
+        container.add(gpanel);
+
+        gframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gframe.setSize(400,600);
+        gframe.setVisible(true);
+
+
+    }
 }
--- src/main/sh/UI/View.java
+++ src/main/sh/UI/View.java
@@ -1,4 +1,29 @@
 package UI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class View {
+
+    JFrame vframe;
+    JTextField vtf;
+    JButton vbutton;
+    JPanel vpanel;
+
+    public void createAndShowViewGUI() {
+        vframe = new JFrame("查看库存");
+        vtf = new JTextField("请输入查询时间段,格式为2010/01/01-2011/01/01");
+        vbutton = new JButton("今日库存盘点");
+        vpanel = new JPanel();
+
+        Container container = vframe.getContentPane();
+        container.setLayout(new FlowLayout());
+        container.add(vpanel);
+        vpanel.add(vtf);
+        vpanel.add(vbutton);
+
+        vtf.setEditable(true);
+        vframe.setSize(400, 400);
+        vframe.setVisible(true);
+    }
 }
