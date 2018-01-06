d02e3d4039e29e599b0afafdfe97be7103b40468
cryinggiraffe <490949304@qq.com>
2018-01-06 12:09:18 +0800

    login

--- src/main/java/DataService/DataServiceImpl/LoginDataServiceImpl.java
+++ src/main/java/DataService/DataServiceImpl/LoginDataServiceImpl.java
@@ -1,17 +1,17 @@
 package DataService.DataServiceImpl;
 
+import DataService.LoginDataService;
 import JDBC.DAO;
-import PO.UserPo;
-import dataService.LoginDataService;
+import PO.UserPO;
 
 public class LoginDataServiceImpl implements LoginDataService{
 
-	DAO<UserPo> dao = new DAO<>();
+	DAO<UserPO> dao = new DAO<>();
 	@Override
 	public String getPassword(String username) {
 		// TODO Auto-generated method stub
 		String sql = "select username, password from User where username = ?";
-		UserPo userPo = dao.get(UserPo.class, sql, username);
+		UserPO userPo = dao.get(UserPO.class, sql, username);
 		return userPo.getPassword();
 	}
 
