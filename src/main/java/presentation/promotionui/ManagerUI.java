6368556a522171b2a1ba99dff4a14db0a861f05d
XuHe <mf1732148@smail.nju.edu.cn>
2018-01-09 12:09:01 +0800

    add managerUI

--- src/main/java/presentation/promotionui/ManagerUI.java
+++ src/main/java/presentation/promotionui/ManagerUI.java
@@ -1,39 +1,37 @@
-package presentation.accountui;
+package presentation.promotionui;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FinancialUI extends JFrame{
+public class ManagerUI extends JFrame {
     private static JButton bt_exit; //登出按钮
     private static JLabel jl_user; //当前用户
     private static JLabel jl_username; //当前用户姓名
-    private static JLabel jl_1;//财务员的版面
-    private static JFrame jf_1;//财务员界面的框架
+    private static JLabel jl_1;//总经理的版面
+    private static JFrame jf_1;//总经理界面的框架
 
-    private static JButton bt_account;//账户管理按钮
-    private static JButton bt_bill;//财务管理按钮
-    private static JButton bt_initialAccount;//期初建账按钮
+    private static JButton bt_promotion;//制定促销策略按钮
+    private static JButton bt_approve;//审批按钮
     private static JButton bt_busiProcess;//经营历程按钮
     private static JButton bt_transSituation;//经营情况按钮
     private static JButton bt_saleSituation;//销售情况按钮
 
-
     private static int Width = 900;
     private static int Height = 700;
 
-    public FinancialUI () {//初始化
+    public ManagerUI () {//初始化
         Font font =new Font("微软雅黑", Font.PLAIN, 15);//设置字体1
         jf_1=new JFrame("财务人员界面");
         jf_1.setSize(Width, Height);
 
         //标题栏
-        jl_user = new JLabel("当前用户：财务员 ");
+        jl_user = new JLabel("当前用户：总经理 ");
         jl_user.setBounds(450,30,140,30);
         jl_user.setFont(font);
 
-        jl_username = new JLabel("01001");//此处字符串后期变为获取到的用户名
+        jl_username = new JLabel("00001");//此处字符串后期变为获取到的用户名
         jl_username.setBounds(580,30,120,30);
         jl_username.setFont(font);
 
@@ -43,17 +41,13 @@ public class FinancialUI extends JFrame{
 
         //设置界面的按钮
         Font font1 =new Font("微软雅黑", Font.PLAIN, 20);//设置按钮字体1
-        bt_account = new JButton("账户管理");
-        bt_account.setBounds(270,250,120,50);
-        bt_account.setFont(font1);
+        bt_promotion = new JButton("制定促销");
+        bt_promotion.setBounds(270,250,120,50);
+        bt_promotion.setFont(font1);
 
-        bt_bill = new JButton("财务管理");
-        bt_bill.setBounds(270,320,120,50);
-        bt_bill.setFont(font1);
-
-        bt_initialAccount = new JButton("期初建账");
-        bt_initialAccount.setBounds(270,390,120,50);
-        bt_initialAccount.setFont(font1);
+        bt_approve = new JButton("审批单据");
+        bt_approve.setBounds(270,320,120,50);
+        bt_approve.setFont(font1);
 
         bt_busiProcess = new JButton("经营历程");
         bt_busiProcess.setBounds(510,250,120,50);
@@ -76,9 +70,8 @@ public class FinancialUI extends JFrame{
         contentPane.add(jl_username);
         contentPane.add(bt_exit);
 
-        contentPane.add(bt_account);
-        contentPane.add(bt_bill);
-        contentPane.add(bt_initialAccount);
+        contentPane.add(bt_promotion);
+        contentPane.add(bt_approve);
         contentPane.add(bt_busiProcess);
         contentPane.add(bt_transSituation);
         contentPane.add(bt_saleSituation);
@@ -101,41 +94,29 @@ public class FinancialUI extends JFrame{
         };
         bt_exit.addActionListener(btExit_ls);
 
-        //账户管理事件处理
-        ActionListener btAccount_ls=new ActionListener() {
+        //制定促销事件处理
+        ActionListener btPromotion_ls=new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 // TODO Auto-generated method stub
-                System.out.println("into account manage");
+                System.out.println("into promotion manage");
                 //mainLayout ml=new mainLayout(jf_1);//为跳转的界面
                 jf_1.setVisible(false);//隐藏当前界面
             }
         };
-        bt_account.addActionListener(btAccount_ls);
+        bt_promotion.addActionListener(btPromotion_ls);
 
-        //财务管理事件处理
-        ActionListener btBill_ls=new ActionListener() {
+        //审批单据事件处理
+        ActionListener btApprove_ls=new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 // TODO Auto-generated method stub
-                System.out.println("into bill manage");
+                System.out.println("into approve manage");
                 //mainLayout ml=new mainLayout(jf_1);//为跳转的界面
                 jf_1.setVisible(false);//隐藏当前界面
             }
         };
-        bt_bill.addActionListener(btBill_ls);
-
-        //期初建账事件处理
-        ActionListener btInitialAccount_ls=new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                // TODO Auto-generated method stub
-                System.out.println("into initial account manage");
-                //mainLayout ml=new mainLayout(jf_1);//为跳转的界面
-                jf_1.setVisible(false);//隐藏当前界面
-            }
-        };
-        bt_initialAccount.addActionListener(btInitialAccount_ls);
+        bt_approve.addActionListener(btApprove_ls);
 
         //经营历程事件处理
         ActionListener btBusiProcess_ls=new ActionListener() {
@@ -172,11 +153,12 @@ public class FinancialUI extends JFrame{
             }
         };
         bt_saleSituation.addActionListener(btSaleSituation_ls);
+
     }
 
     public static void main (String[] args) {
         //初始化
-        FinancialUI financialUI = new FinancialUI();
+        ManagerUI managerUI = new ManagerUI();
     }
 
 }
