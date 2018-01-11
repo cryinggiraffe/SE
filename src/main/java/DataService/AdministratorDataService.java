12f9dbd1cace062326295d0aa138898b00928742
cryinggiraffe <490949304@qq.com>
2018-01-11 16:28:06 +0800

    user

--- src/main/java/DataService/AdministratorDataService.java
+++ src/main/java/DataService/AdministratorDataService.java
@@ -6,5 +6,7 @@ public interface AdministratorDataService {
 	
 	public boolean insert(UserPO userPO);
 	public boolean delete(String username);
+	public boolean update(UserPO userPO);
+	public UserPO getUser(String username);
 	
 }
