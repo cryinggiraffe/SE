7c3313e9c62919708706e1813070defc47ca6347
cryinggiraffe <490949304@qq.com>
2018-01-13 01:26:38 +0800

    salesituation

--- src/main/java/DataService/DataServiceImpl/SaleSituationDataServiceImpl.java
+++ src/main/java/DataService/DataServiceImpl/SaleSituationDataServiceImpl.java
@@ -14,32 +14,41 @@ public class SaleSituationDataServiceImpl implements SaleSituationDataService{
 	@Override
 	public List<SaleSituationPo> findForTime(Date begin, Date end) {
 		// TODO Auto-generated method stub
-		String sql = "";
+		String sql = "select date, name, version, quantity, pirce,subtotalprice from SaleForm,SaleCommodity where "
+				+ " SaleCommodity.saleformid = SaleForm.id and date >=? and date <= ?";
 		return dao.getALL(SaleSituationPo.class, sql, begin, end);
 	}
 
 	@Override
 	public List<SaleSituationPo> findForName(String name) {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select date, name, version, quantity, pirce,subtotalprice from SaleForm,SaleCommodity where "
+				+ " SaleCommodity.saleformid = SaleForm.id and name = ?";
+		return dao.getALL(SaleSituationPo.class, sql, name);
 	}
 
 	@Override
 	public List<SaleSituationPo> findForClient(String client) {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select date, name, version, quantity, pirce,subtotalprice from SaleForm,SaleCommodity where "
+				+ " SaleCommodity.saleformid = SaleForm.id and client = ?";
+		return dao.getALL(SaleSituationPo.class, sql, client);
 	}
 
 	@Override
 	public List<SaleSituationPo> findForSalesman(String salesman) {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select date, name, version, quantity, pirce,subtotalprice from SaleForm,SaleCommodity where "
+				+ " SaleCommodity.saleformid = SaleForm.id and salesman = ?";
+		return dao.getALL(SaleSituationPo.class, sql, salesman);
 	}
 
 	@Override
 	public List<SaleSituationPo> findForHouseWare(String houseware) {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "select date, name, version, quantity, pirce,subtotalprice from SaleForm,SaleCommodity where "
+				+ " SaleCommodity.saleformid = SaleForm.id and houseware = ?";
+		return dao.getALL(SaleSituationPo.class, sql, houseware);
 	}
 
 }
