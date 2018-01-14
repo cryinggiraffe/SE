63826135e669a975000a03c8529b1550256d3ab7
XuHe <mf1732148@smail.nju.edu.cn>
2018-01-14 14:06:38 +0800

    fix bug

--- src/main/java/DataService/DataServiceImpl/ImportDataServiceImpl.java
+++ src/main/java/DataService/DataServiceImpl/ImportDataServiceImpl.java
@@ -26,7 +26,7 @@ public class ImportDataServiceImpl implements ImportDataService{
 	public boolean update(ImportFormPO importFormPO) {
 		// TODO Auto-generated method stub
 		String sql = "update ImportForm set provider = ？, houseware = ？, operator = ？, remark = ？, sum = ？, state = ？, date = ？where id = ?";
-		return dao.update(sql, importFormPO.getProvider(), importFormPO.getHouseware(), importFormPO.getOperator(), 
+		return dao.update(sql, importFormPO.getClient(), importFormPO.getHouseware(), importFormPO.getOperator(),
 		          			   importFormPO.getRemark(), importFormPO.getSum(),importFormPO.getState(), importFormPO.getDate());
 	}
 
