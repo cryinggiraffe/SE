fcf5a80a207bf9d6cc0b0ea1d2c777874ab81780
蒲瑜琦 <mf1732106@smail.nju.edu.cn>
2018-01-14 19:57:48 +0800

    kk

--- src/main/java/blService/ImportFormService.java
+++ src/main/java/blService/ImportFormService.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 public interface ImportFormService {
-    void newForm(String FormType/*进货单JHD 进货退货单JHTHD*/,String provider, String houseware, String operator,
+    String newForm(String FormType/*进货单JHD 进货退货单JHTHD*/,String provider, String houseware, String operator,
                  List<Commodity> importcommoditylist, String remark, double sum, String state, Date date,List<Commodity> list);
     ImportFormPO findById(String id);
     
