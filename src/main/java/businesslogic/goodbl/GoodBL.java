65f521d4d911dc4cc79234bdbf4991b91819444c
luyao <yavenika@163.com>
2018-01-15 12:28:54 +0800

    ly

--- src/main/java/businesslogic/goodbl/GoodBL.java
+++ src/main/java/businesslogic/goodbl/GoodBL.java
@@ -8,10 +8,6 @@ import PO.GoodPO;
 import PO.InventoryPO;
 import PO.LossPO;
 import PO.OverflowPO;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
