50b173285308b9789eebcab7ddbb22077a3a8ce6
蒲瑜琦 <mf1732106@smail.nju.edu.cn>
2018-01-16 16:35:07 +0800

    kk

--- src/main/java/businesslogic/categorybl/CategoryBL.java
+++ src/main/java/businesslogic/categorybl/CategoryBL.java
@@ -3,12 +3,18 @@ import DataService.DataServiceImpl.CategoryDataServiceImpl;
 import PO.CategoryPO;
 
 import java.util.List;
+import java.util.Random;
 
 public class CategoryBL {
     private CategoryDataServiceImpl cds=new CategoryDataServiceImpl();
     public void addCategory(CategoryPO po){
         cds.insert(po);
 
+    }
+    public String generateId(){
+        Random r=new Random();
+
+        return String.valueOf(r.nextInt(999999999));
     }
     public void deleteCategory(CategoryPO po){
         cds.delete(po);
