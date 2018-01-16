90672381b3dff996e7fe502fea381b7ff4deccb9
cryinggiraffe <490949304@qq.com>
2018-01-16 15:17:06 +0800

    stock

--- src/main/java/DataService/DataServiceImpl/GiftDataServiceImpl.java
+++ src/main/java/DataService/DataServiceImpl/GiftDataServiceImpl.java
@@ -13,8 +13,8 @@ public class GiftDataServiceImpl implements GiftDataService{
 	@Override
 	public boolean insert(GiftPO giftPO) {
 		// TODO Auto-generated method stub
-		String sql = "insert into Gift(giftlistid, giftid, name, price, num, state, date) values(?,?,?,?,?,?,?)";
-		return dao.update(sql, giftPO.getGiftlistid(), giftPO.getGiftid(), giftPO.getName(),
+		String sql = "insert into Gift(giftlistid, goodid, goodname, price, num, state, date) values(?,?,?,?,?,?,?)";
+		return dao.update(sql, giftPO.getGiftlistid(), giftPO.getGoodid(), giftPO.getGoodname(),
 				               giftPO.getPrice(), giftPO.getNum(), giftPO.getState(),
 				               giftPO.getDate());
 	}
@@ -30,33 +30,33 @@ public class GiftDataServiceImpl implements GiftDataService{
 	@Override
 	public boolean update(GiftPO giftPO) {
 		// TODO Auto-generated method stub
-		String sql = "update Gift set  name = ?, price = ? , num = ?, state = ? "
-			     + ", date = ? where giftlistid = ? and giftid = ?";
-		return dao.update(sql, giftPO.getName(), giftPO.getPrice(), giftPO.getNum(), giftPO.getState(),
-	               			   giftPO.getDate(), giftPO.getGiftlistid(), giftPO.getGiftid());
+		String sql = "update Gift set  goodname = ?, price = ? , num = ?, state = ? "
+			     + ", date = ? where giftlistid = ? and goodid = ?";
+		return dao.update(sql, giftPO.getGoodname(), giftPO.getPrice(), giftPO.getNum(), giftPO.getState(),
+	               			   giftPO.getDate(), giftPO.getGiftlistid(), giftPO.getGoodid());
 	}
 	
 	@Override
 	public boolean updateliststate(GiftPO giftPO) {
 		// TODO Auto-generated method stub
-		String sql = "update Gift set  name = ?, price = ? , num = ?, state = ? "
+		String sql = "update Gift set  goodname = ?, price = ? , num = ?, state = ? "
 				     + ", date = ? where giftlistid = ?";
 		
-		return dao.update(sql, giftPO.getName(),giftPO.getPrice(), giftPO.getNum(), giftPO.getState(),
+		return dao.update(sql, giftPO.getGoodname(),giftPO.getPrice(), giftPO.getNum(), giftPO.getState(),
 	               			   giftPO.getDate(),giftPO.getGiftlistid());
 	}
 
 	@Override
 	public List<GiftPO> list(String giftlistid) {
 		// TODO Auto-generated method stub
-		String sql = "select giftlistid, giftid, name, price, num, state, date from Gift where giftlistid = ?";
+		String sql = "select giftlistid, goodid, goodname, price, num, state, date from Gift where giftlistid = ?";
 		return dao.getALL(GiftPO.class, sql, giftlistid);
 	}
 
 	@Override
 	public List<GiftPO> findForType() {
 		// TODO Auto-generated method stub
-		String sql = "select giftlistid, giftid, name, price, num, state, date from Gift";
+		String sql = "select giftlistid, goodid, goodname, price, num, state, date from Gift";
 		return dao.getALL(GiftPO.class, sql);
 	}
 
